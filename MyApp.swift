18265593798f4253f3675b6c13219ed55d2cab88
Nelson Taruc <nelson.taruc@lextech.com>
2025-05-28 15:11:15 -0500

    Add files via upload

---
+++ MyApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct SpiderSolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//                .windowResizability(.contentSize)
+  //              .windowStyle(.automatic)
+        }
+    }
+}
